89f328e394b2d9db2e249acc772473035a21adb9
Sebastian Morales <sebasandres.1547@gmail.com>
2026-02-26 11:12:18 -0500

    feat: crear estructura base del Main con ArrayLists

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,18 @@
 package org.example;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
     static void main() {
 
+        ArrayList<Estudiantes> estudiantes = new ArrayList<>();
+        ArrayList<Asignatura> asignaturas = new ArrayList<>();
+        ArrayList<Nota> Notas = new ArrayList<>();
+
         }
+
     }
-}
+
