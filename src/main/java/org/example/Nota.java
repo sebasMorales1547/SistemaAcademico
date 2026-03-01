d4efd4104b552c04c8406bcd15788e71e6eb75e8
Alfonso David Araujo <alfonsodaraujo@unicesa.edu.co>
2026-03-01 18:37:41 -0500

    feat:crear clase Nota con atributos

---
+++ src/main/java/org/example/Nota.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.example;
+
+/**
+ *
+ * @author Familia
+ */
+public class Nota {
+    
+// Atributos
+    private Estudiante estudiante;
+    private Asignatura asignatura;
+    private double valor;
+    private String periodo;
