fb4bf92bd3dc3ca23c3d68a8bbacb00849273767
Juan Diego Sierra <jdiegosierra@unicesar.edu.co>
2026-03-01 11:20:31 -0500

    feat: agregar encapsulamiento a clase Estudiante

--- src/main/java/Estudiante/ESTUDIANTE.java
+++ src/main/java/Estudiante/ESTUDIANTE.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package org.example;
+package Estudiante;
 
 /**
  *
  * @author PC
  */
-public class ESTUDIANTE {
+public class Estudiante {
     private String nombre;
     private float codigo; //Numnero de identificacion.
     private String Programa;
