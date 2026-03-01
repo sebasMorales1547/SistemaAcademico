f2a9c99ac35a3133a65fd501346f66ae03e4237f
Juan Diego Sierra <jdiegosierra@unicesar.edu.co>
2026-03-01 11:21:53 -0500

    feat: agregar encapsulamiento a clase Estudiante

--- src/main/java/Estudiante/ESTUDIANTE.java
+++ src/main/java/Estudiante/ESTUDIANTE.java
@@ -14,5 +14,53 @@ public class Estudiante {
     private String Programa;
     private int semestre;
     
+    
+    
+   public Estudiante(){
+       
+   }
+   
+    public Estudiante(String nombre, float codigo, String Programa, int semestre) {
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.Programa = Programa;
+        this.semestre = semestre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public float getCodigo() {
+        return codigo;
+    }
+
+    public String getPrograma() {
+        return Programa;
+    }
+
+    public int getSemestre() {
+        return semestre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCodigo(float codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setPrograma(String Programa) {
+        this.Programa = Programa;
+    }
+
+    public void setSemestre(int semestre) {
+        this.semestre = semestre;
+    }
+    
+    
+    
+    
    
 }
