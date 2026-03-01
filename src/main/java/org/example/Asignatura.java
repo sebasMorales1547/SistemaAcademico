6bcc24ab48c985d286272a395b6f0c6fed7a2d36
Alfonso David Araujo <alfonsodaraujo@unicesa.edu.co>
2026-03-01 18:24:50 -0500

    feat: crear clase Asignatura con atributos

---
+++ src/main/java/org/example/Asignatura.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.example;
+
+/**
+ *
+ * @author Familia
+ */
+public class Asignatura {
+
+    // Atributitos
+    private String codigo;
+    private String nombre;
+    private int creditos;
+    private String docente;
