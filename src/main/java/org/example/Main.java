0695043442c4b6fe6f31bcf03fa2e4d6a89c3400
Sebastian Morales <sebasandres.1547@gmail.com>
2026-03-01 18:57:54 -0500

    feat: implementar CREATE - registrar estudiante

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,14 @@
 package org.example;
 import java.util.Scanner;
 import java.util.ArrayList;
-
+import Estudiante.Estudiante;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-   // static ArrayList<Estudiantes> estudiantes = new ArrayList<>();
-   // static ArrayList<Asignatura> asignaturas = new ArrayList<>();
-   // static ArrayList<Nota> Notas = new ArrayList<>();
+    static ArrayList<Estudiantes> estudiantes = new ArrayList<>();
+    static ArrayList<Asignatura> asignaturas = new ArrayList<>();
+    static ArrayList<Nota> Notas = new ArrayList<>();
 
     Scanner leer = new Scanner(System.in);
 
@@ -68,6 +68,31 @@ public class Main {
 
         }while (opcionEstudiante != 6);
 
+    }
+
+    public void registrarEstudiante(){
+
+        System.out.println("Ingrese en nombre del estudiante");
+        String nombre = leer.nextLine();
+
+        System.out.println("Ingrese el apellido del estudiante");
+        String apellido = leer.nextLine();
+
+        System.out.println("Ingrese el codigo del estudiante");
+        String codigo = leer.nextLine();
+
+        System.out.println("Ingrese el semestre del estudiante");
+        int semestre = leer.nextInt();
+
+        System.out.println("Ingrese la edad del estudiante");
+        int edad = leer.nextInt();
+
+        //Crear el objeto
+        Estudiante nuevoEstudiante = new Estudiante(nombre, apellido, codigo, semestre, edad);
+
+        //añadir el objeto al arrayList
+        estudiantes.add(nuevoEstudiante);
+        System.out.println("El estudiante ha sido registrado correctamente");
     }
 
 
