d35ef125af1c9ee08964f94e45cf026e641d35dc
Alfonso David Araujo <alfonsodaraujo@unicesa.edu.co>
2026-03-01 18:34:57 -0500

    feat: agregar toString a clase Asignatura

--- src/main/java/org/example/Asignatura.java
+++ src/main/java/org/example/Asignatura.java
@@ -61,3 +61,14 @@ public class Asignatura {
     public void setDocente(String docente) {
         this.docente = docente;
     }
+    
+@Override
+    public String toString() {
+        return "Asignatura{" +
+                "codigo='" + codigo + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", creditos=" + creditos +
+                ", docente='" + docente + '\'' +
+                '}';
+    }
+}
