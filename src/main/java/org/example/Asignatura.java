f8b257aaa15f15d55931fa920098d72616938169
Alfonso David Araujo <alfonsodaraujo@unicesa.edu.co>
2026-03-01 18:32:56 -0500

    feat: agregar encapsulamiento aclase asignatura

--- src/main/java/org/example/Asignatura.java
+++ src/main/java/org/example/Asignatura.java
@@ -15,3 +15,49 @@ public class Asignatura {
     private String nombre;
     private int creditos;
     private String docente;
+
+// Constructor
+    public Asignatura() {
+    }
+
+    // Constructor con sus debidos parametros
+    public Asignatura(String codigo, String nombre, int creditos, String docente) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.creditos = creditos;
+        this.docente = docente;
+    }
+
+    // Debidos Getters
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getCreditos() {
+        return creditos;
+    }
+
+    public String getDocente() {
+        return docente;
+    }
+
+    // Debidos Setters
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCreditos(int creditos) {
+        this.creditos = creditos;
+    }
+
+    public void setDocente(String docente) {
+        this.docente = docente;
+    }
