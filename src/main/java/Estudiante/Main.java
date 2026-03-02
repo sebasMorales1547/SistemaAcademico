23369c5d0bb0d3d71995168fb745ef6b93911990
Juan Diego Sierra <jdiegosierra@unicesar.edu.co>
2026-03-01 21:36:01 -0500

    feat: implementar CREATE - registrar asignatura

--- src/main/java/Estudiante/Main.java
+++ src/main/java/Estudiante/Main.java
@@ -1,14 +1,38 @@
 package Estudiante;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
 
-        }
+    static Scanner sc = new Scanner(System.in);
+
+    public static void registrarAsignatura() {
+
+        System.out.println("=== Registrar Asignatura ===");
+
+        System.out.print("Ingrese el codigo: ");
+        String codigo = sc.nextLine();
+
+        System.out.print("Ingrese el nombre: ");
+        String nombre = sc.nextLine();
+
+        System.out.print("Ingrese los creditos: ");
+        int creditos = Integer.parseInt(sc.nextLine());
+
+        System.out.print("Ingrese el docente: ");
+        String docente = sc.nextLine();
+
+        System.out.println("\nAsignatura registrada correctamente:");
+        System.out.println("Codigo: " + codigo);
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Creditos: " + creditos);
+        System.out.println("Docente: " + docente);
     }
 
+    public static void main(String[] args) {
+        registrarAsignatura();
+    }
+    
+    
+}
 
